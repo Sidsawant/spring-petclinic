eaf3f6feb18631e2617095ef508f86098e618ecc
ST\Siddharth.Sawant <siddharth.sawant@sterlingts.com>
2017-05-08 16:40:23 +0530

    testing1

--- pipeline/java/maven.groovy
+++ pipeline/java/maven.groovy
@@ -3,8 +3,7 @@
 def build(def args){
     
 	
-    ${tool 'maven311'} clean install -Dmaven.test.skip=true  -Daws.account=1111 -Daws.image.version=222 -Daws.region=3333 -P aws-deploy -U -e
-    
+    ${tool 'maven311'} mvn clean install -Dmaven.test.skip=true  
 }
 
 return this;
